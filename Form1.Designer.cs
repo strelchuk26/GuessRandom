8f8a2da81de7dbc5f25cfe6e26957f4d74875040
Bogdan <bogda026@gmail.com>
2023-02-03 18:30:33 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GuessRandom
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.Font = new System.Drawing.Font("Gilroy Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button1.Location = new System.Drawing.Point(210, 257);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(154, 58);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "START";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Gilroy ExtraBold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(135, 121);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(524, 59);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "GUESS YOUR NUMBER!";
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.White;
+			this.button2.Font = new System.Drawing.Font("Gilroy Black", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.button2.Location = new System.Drawing.Point(439, 257);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(154, 58);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "EXIT";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(42)))), ((int)(((byte)(115)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Guess your number!";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessRandom
+{
+
+	public partial class Form1 : Form
+	{
+		Random rnd = new Random();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			bool guessed = false;
+			int startNum = 1;
+			int endNum = 100;
+
+			while (guessed != true)
+			{
+				int num = rnd.Next(startNum, endNum);
+				int range = endNum - startNum;
+				var result = MessageBox.Show($"Is your number higher than {num}?",
+											 "Guess your number!",
+											 MessageBoxButtons.YesNo,
+											 MessageBoxIcon.Question);
+
+				if (range == 1)
+				{
+					MessageBox.Show($"Your number is {num}!");
+					guessed = true;
+				}
+				else
+				{
+					if (result == DialogResult.Yes)
+					{
+						startNum = num;
+					}
+					else if (result == DialogResult.No)
+					{
+						endNum = num;
+					}
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
